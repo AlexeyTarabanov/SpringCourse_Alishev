15acea048d9a3810a16dbd333940f4b2353c1f9e
Alexey Tarabanov <tarabanov@bk.ru>
2020-12-13 00:06:43 +0300

    add 6 lesson

--- spring-app1/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,13 +1,41 @@
 package ru.alishev.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+
+    //private Music music;
+    List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer() {}
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }

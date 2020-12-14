d0d3c625e8b0c75b17a07f0cab41e4c5a4dca3cb
Alexey Tarabanov <tarabanov@bk.ru>
2020-12-15 00:36:11 +0300

    add 8 lesson

--- spring-app1/src/main/java/ru/alishev/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -8,21 +8,24 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusic();
+        ClassicalMusic classicalMusic = context.getBean("musicBeanClassicalMusic", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(comparison);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer .getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        //musicPlayer.playMusic();
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer .getVolume());
 
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
